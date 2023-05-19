dd4fc1a40f546139f440542c85f01bf63fe2d564
Sergei Uzbekov <uzserge@gmail.com>
2023-05-19 17:54:20 +0300

    AI bulk translate, retries, update/insert, README

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# insraeli-bank-telegram-notifier
+# **ShekelStreamer**
+
+This project uses a series of configurable environment variables to automatically scrape financial transactions, store them in a MongoDB database, translate their descriptions using the OpenAI API, and finally send them to specified Telegram channels. This system uses a cron job to regularly pull new transactions, which are processed and stored.
+
+## **Features**
+
+- Automated financial transactions scraping from specified providers
+- Storing transactions in a MongoDB database
+- Sending transaction notifications to specified Telegram channels
+- Customizable schedule for getting new transactions
+- Support for multiple users and separate credentials for each financial provider
+- Translating transactions descriptions using the OpenAI API, catering to the Israeli context and supporting custom phrase recognition (optional)
+
+## **Parameters**
+
+| Parameter | Description | Example |
+| --- | --- | --- |
+| MONGO_CONNECTION_STRING | MongoDB connection string | mongodb+srv://user:password@cluster0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority |
+| TELEGRAM_BOT_TOKEN | Telegram bot token for sending notifications | 123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11 |
+| TRANSACTION_SYNC_SCHEDULE | Cron schedule for getting new transactions | 0 8 * * * |
+| USERS | Comma-separated list of users to scrape | USER1,USER2,JOHN,MARY |
+| USERX_TELEGRAM_CHANNEL_ID | Telegram channel ID for user X | -1001234567890 |
+| USERX_COMPANY_Y_CREDENTIALS | User X's credentials for company Y | JOHN_HAPOALIM_USER_CODE |
+| OPENAI_API_KEY | OpenAI API key for translating transactions' descriptions | sk-key1234 |
+| GPT_MODEL_FAST | GPT model to use for translation | gpt-3.5-turbo |
+| GPT_TRANSLATION_PROMPT | Translation request to GPT | Your translation prompt |
+
+For USERX_COMPANY_Y_CREDENTIALS, replace USERX with the user's name and COMPANY_Y with the company name. The company should match one of the providers listed [here](https://github.com/eshaham/israeli-bank-scrapers/blob/6b961fd7318cc522ac12de83498c1e6c2316ac68/src/definitions.ts#L5). You can find descriptions and formats for the required credentials in the [documentation](https://github.com/eshaham/israeli-bank-scrapers/blob/master/README.md#specific-definitions-per-scraper).
+
+## **Usage and Configuration**
+
+1. Copy the provided `.env.example` file to a new file named `.env`.
+2. Fill in the required values in the `.env` file.
+3. Run the application with the configured environment variables.
+4. The application will start automatically scraping transactions according to the specified cron schedule.
+
+Please note that if the TELEGRAM_BOT_TOKEN is not set, transactions will only be saved to the database without sending any notifications. If the OpenAI API key is not set, transactions will be saved and sent without translated descriptions.
+
+If you want to configure notifications for a new user, you must set the USERX_TELEGRAM_CHANNEL_ID and USERX_COMPANY_Y_CREDENTIALS parameters, where USERX is the user's name and COMPANY_Y is the company name. Each user can have separate credentials for each financial provider.
+
+For further information, refer to the `.env.example` file provided in the repository.
+
+### About translation using the OpenAI API
+
+The translation process is designed taking into account the Israeli context and allows you to recognize custom phrases. For example, the translation process could use the following prompt given in the file `.env.example`:
+
+```plaintext
+You are a translation service.
+Use the list of correct translations of some sentences when translating (format: "original text|translation"):
+
+פועלים-|Hapoalim
+אושר עד|Osher Ad
+
+Please provide the translations for each line of text, plain list without original text.
+Here is an example request:
+מסטרקרד
+מסטרקרד
+
+And the corresponding response:
+Mastercard
+Mastercard
+
+Now, translate into English every following line of text, specifically in the Israeli context:
+מסטרקרד
+<text_to_replace>
+Respond in a one-column TSV format.
+```
+
+In this example, `<text_to_replace>` is a placeholder where the original descriptions of transactions will be placed for translation. You can add a list of custom phrases and their correct translations to the prompt. The system will then recognize these phrases and translate them accordingly.
+
+## **Contributing**
+
+Please feel free to submit issues or pull requests for any improvements or bug fixes. Your contributions are always welcome!
+
+## **License**
+
+This project is licensed under the MIT License.
--- index.js
+++ index.js
@@ -9,7 +9,11 @@ import moment from 'moment-timezone';
 import { ChatGPTAPI } from 'chatgpt';
 import retry from 'async-retry';
 
-// Function to format transaction for Telegram
+/**
+ * Function to format transaction for Telegram
+ * @param {any} transaction 
+ * @returns {string} Formatted transaction
+ */
 function format(transaction) {
   let date = moment(transaction.date).tz("Asia/Jerusalem");
   let processedDate = moment(transaction.processedDate).tz("Asia/Jerusalem");
@@ -34,8 +38,20 @@ Status: ${transaction.status}
 `;
 }
 
-// Function to translate transaction's description
-async function translateDescription(description) {
+/**
+ * Function to translate transactions' descriptions using OpenAI Chat API
+ * @param {string[]} descriptions
+ * @returns {Promise<string[]>} Translated descriptions
+ */
+async function translateDescriptions(descriptions) {
+  const placeholder = '<text_to_replace>';
+
+  if (!process.env.OPENAI_API_KEY || !process.env.GPT_MODEL_FAST || !process.env.GPT_TRANSLATION_PROMPT
+    || !process.env.GPT_TRANSLATION_PROMPT.includes(placeholder)) {
+    console.log('No OpenAI API key or GPT model or GPT translation prompt found. Skipping translation.');
+    return descriptions.map(_ => null);
+  }
+
   const api = new ChatGPTAPI({
     apiKey: process.env.OPENAI_API_KEY,
     completionParams: {
@@ -44,62 +60,180 @@ async function translateDescription(description) {
     }
   });
 
-  const request = process.env.GPT_TRANSLATION_REQUEST.replace(/\\n/g, '\n').replace('<text_to_replace>', description);
-  const response = await api.sendMessage(request)
-  console.log(`Translation response:\n${response.text}`);
+  // Join all descriptions into one string
+  const description = descriptions.join('\n');
+
+  const prompt = process.env.GPT_TRANSLATION_PROMPT.replace(/\\n/g, '\n').replace(placeholder, description);
+  const response = await api.sendMessage(prompt)
+
+  console.log(`Request:\n${prompt}\n\nResponse:\n${response.text}\n`);
 
   // Split the text into lines and obtain translations from the response in the format:
   // "translate1
   // translate2"
   // Note: The first line of the response corresponds to the translation of the first phrase from the request.
-  // This is why we start the index from [1].
-  // This is done to form a list of phrases that can be more clearly understood by the GPT.
+  // This is why the first line is ignored (traslations.slice(1)).
+  // This is done to form a list of phrases that can be more clearly understood by the GPT when there is only one phrase.
   let traslations = response.text.split('\n');
-  return traslations[1];
+  if (traslations.length !== descriptions.length + 1) {
+    throw new Error(`Number of translations (${traslations.length}) does not match number of descriptions (${descriptions.length})`);
+  }
+
+  return traslations.slice(1);
 }
 
-// Function to check if transaction already exists in db
-async function transactionExists(transaction) {
+/**
+ * Function to get list of translations for given descriptions
+ * @param {any[]} transaction
+ * @returns {Promise<string[]>} Translated descriptions
+ */
+async function getTranslations(transactions) {
+  let descriptionsToTranslate = [];
+  transactions.forEach((transaction) => {
+    let description = transaction.memo ? `${transaction.description} - ${transaction.memo}` : transaction.description;
+    descriptionsToTranslate.push(description);
+  });
+
+  try {
+    // Fetch translations in bulk
+    const translations = await retry(async () => {
+      return await translateDescriptions(descriptionsToTranslate);
+    }, {
+      retries: 5,
+      factor: 2,
+      minTimeout: 20000,
+      randomize: true
+    });
+
+    return translations;
+  } catch (err) {
+    // If the request still fails after all retries, log the error
+    console.error(`Failed to translate descriptions: ${err}`);
+  }
+}
+
+/**
+ * Function to get existing transactions from db (by date, chargedAmount, description, processedDate, status and translatedDescription)
+ * @param {any} transactions 
+ * @returns {Promise<any[]>} Set of transaction keys (date+chargedAmount+description+processedDate+status)
+ */
+async function getExistingTransactions(transactions) {
   const client = new MongoClient(process.env.MONGO_CONNECTION_STRING);
   try {
     await client.connect();
     const database = client.db("transactionsDB");
-    const transactions = database.collection("transactions");
-    // Check if transaction already exists in database
-    const existingTransaction = await transactions.findOne({
+    const transCollection = database.collection("transactions");
+
+    const query = {
+      $or: transactions.map(transaction => ({
+        date: transaction.date,
+        chargedAmount: transaction.chargedAmount,
+        description: transaction.description,
+
+        // following fields will update if transaction already exists, so we need to check them too
+        processedDate: transaction.processedDate,
+        status: transaction.status,
+        translatedDescription: { $not: { $eq: null } }
+      }))
+    };
+
+    const cursor = transCollection.find(query);
+    const existingTransactions = new Set();
+
+    for await (const doc of cursor) {
+      existingTransactions.add(doc.date + doc.chargedAmount + doc.description + doc.processedDate + doc.status);
+    }
+
+    return existingTransactions;
+
+  } finally {
+    await client.close();
+  }
+}
+
+/**
+ * Function to save or update transaction in db
+ * @param {any} transaction 
+ * @returns {Promise<boolean>} true if transaction was inserted, false if transaction already exists
+ */
+async function saveOrUpdate(transaction) {
+  const client = new MongoClient(process.env.MONGO_CONNECTION_STRING);
+  try {
+    await client.connect();
+    const database = client.db("transactionsDB");
+    const transCollection = database.collection("transactions");
+
+    // Check if transaction already exists
+    const existingTransaction = await transCollection.findOne({
       date: transaction.date,
       chargedAmount: transaction.chargedAmount,
       description: transaction.description
     });
-    return !!existingTransaction;
+
+    if (existingTransaction) {
+      // Transaction exists, update it
+      await transCollection.updateOne(
+        {
+          date: transaction.date,
+          chargedAmount: transaction.chargedAmount,
+          description: transaction.description
+        },
+        {
+          $set: transaction,
+          $currentDate: { updatedAt: true } // Add or update a updatedAt field with the current date
+        }
+      );
+
+      return false;
+    } else {
+      transaction.createdAt = new Date(); // Add a createdAt field with the current date
+      await transCollection.insertOne(transaction);
+
+      return true;
+    }
   } finally {
     await client.close();
   }
 }
 
-async function save(transaction) {
-  const client = new MongoClient(process.env.MONGO_CONNECTION_STRING);
-  try {
-    await client.connect();
-    const database = client.db("transactionsDB");
-    const transactions = database.collection("transactions");
-    await transactions.insertOne(transaction);
-  } finally {
-    await client.close();
+/**
+ * Function to send transaction to Telegram chat with retries
+ * @param {any} transaction 
+ * @param {string} chatId
+ */
+async function notify(transaction, chatId) {
+  if (!process.env.TELEGRAM_BOT_TOKEN || !chatId) {
+    console.log('No Telegram bot token or chat ID found. Skipping notification.');
+    return;
   }
-}
 
-function notify(transaction, chatId) {
   const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
-  bot.sendMessage(chatId, format(transaction), { parse_mode: 'Markdown' });
+
+  try {
+    await retry(async () => {
+      await bot.sendMessage(chatId, format(transaction), { parse_mode: 'Markdown' });
+    }, {
+      retries: 5,
+      minTimeout: 30 * 1000, // 30 seconds
+      randomize: true,
+    });
+  } catch (err) {
+    // If the request still fails after all retries, log the error
+    console.error(`Failed to send message to Telegram: ${err}`);
+  }
 }
 
-// Function to scrape bank transactions, store to MongoDB and send to Telegram chat
-async function handleTransactions(user, companyId, credentials, chatId) {
+/**
+ * Function to scrape bank transactions, store to MongoDB and send to Telegram chat
+ * @param {string} user
+ * @param {CompanyTypes} companyId
+ * @param {any} credentials
+ * @param {string} chatId
+ */
+async function handleTransactions(user, companyId, credentials, chatId, startDate) {
   const scraper = createScraper({
     companyId: companyId,
-    startDate: new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1),
-    //startDate: new Date(new Date().getFullYear() - 1, new Date().getMonth(), new Date().getDate()),
+    startDate: startDate,
     combineInstallments: false,
     timeout: 0 // no timeout
   });
@@ -131,41 +265,51 @@ async function handleTransactions(user, companyId, credentials, chatId) {
       });
     });
 
-    // just last txns
-    // TODO: rework to get actual last txns that was not scraped before
-    transactions = transactions.slice(-5);
+    // Sort transactions by date from oldest to newest
+    transactions.sort((a, b) => a.date - b.date)
 
-    for (const transaction of transactions) {
-      if (await transactionExists(transaction)) {
-        console.log(`Skipping existing transaction: ${JSON.stringify(transaction)}`);
-        continue;
+    const existingTransactions = await getExistingTransactions(transactions);
+
+    // Filter out the transactions that already exist in the db and don't need to be updated
+    transactions = transactions.filter(transaction => {
+      const key = transaction.date + transaction.chargedAmount + transaction.description + transaction.processedDate + transaction.status;
+      return !existingTransactions.has(key);
+    });
+
+    const chunkSize = 20; // to not exceed token limit in OpenAI Chat API while translating
+    let chunkCount = Math.ceil(transactions.length / chunkSize);
+
+    for (let i = 0; i < chunkCount; i++) {
+
+      let currentTransactions = transactions.slice(i * chunkSize, (i + 1) * chunkSize);
+
+      // Create a list of descriptions to translate
+      const translations = await getTranslations(currentTransactions);
+
+      // Assign translations back to current transactions
+      currentTransactions.forEach((transaction, index) => {
+        transaction.translatedDescription = translations[index];
+      });
+
+      for (const transaction of currentTransactions) {
+        const isNewTransaction = !!await saveOrUpdate(transaction);
+        // Send notification only if transaction is new
+        if (isNewTransaction) {
+          await notify(transaction, chatId);
+        }
       }
-
-      // Try to translate description
-      if (process.env.OPENAI_API_KEY && process.env.GPT_TRANSLATION_REQUEST && process.env.GPT_MODEL_FAST) {
-        let description = transaction.memo ? `${transaction.description} - ${transaction.memo}` : transaction.description;
-
-        const translatedDescription = await retry(async () => {
-          return await translateDescription(description);
-        }, {
-          retries: 5,
-          factor: 2,
-          minTimeout: 20000,
-          randomize: true
-        });
-
-        transaction.translatedDescription = translatedDescription;
-      }
-
-      await save(transaction);
-      await notify(transaction, chatId);
     }
-
   } else {
-    console.error(`Scraping failed for the following reason: ${JSON.stringify(scrapeResult)}`);
+    console.error(`Scraping failed for the following reason: ${scrapeResult.errorType} - ${scrapeResult.errorMessage}`);
   }
 }
 
+/**
+ * Function to find key in object, case insensitive
+ * @param {any} object 
+ * @param {string} targetKey
+ * @returns {string} Key of the object that matches the target key, case insensitive
+ */
 function findKeyCaseInsensitive(object, targetKey) {
   const lowerCaseTargetKey = targetKey.toLowerCase();
   for (const key in object) {
@@ -207,8 +351,17 @@ users.forEach((user) => {
       return;
     }
 
+    // a week ago
+    let startDate = new Date(new Date().setDate(new Date().getDate() - 7));
+    // a month ago
+    // let startDate = new Date(new Date().setMonth(new Date().getMonth() - 1));
+    // a year ago
+    // let startDate = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
+
+    handleTransactions(user, CompanyTypes[companyTypeKey], credentials, chatId, startDate);
+
     cron.schedule(process.env.TRANSACTION_SYNC_SCHEDULE, () => {
-      handleTransactions(user, CompanyTypes[companyTypeKey], credentials, chatId);
+      handleTransactions(user, CompanyTypes[companyTypeKey], credentials, chatId, startDate);
     }, {
       timezone: 'Asia/Jerusalem'
     });
